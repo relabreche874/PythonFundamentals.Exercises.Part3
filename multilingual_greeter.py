231944e21594b6f57f31274475eec301512a91aa
Robert <robert@Zipcoders-MacBook-Pro-60.local>
2023-03-20 08:30:32 -0400

    Fixed code

--- multilingual_greeter.py
+++ multilingual_greeter.py
@@ -1,11 +1,11 @@
 def lang_select() :
-    int(choice) = input("Please choose 1-English 2-Japanese 3-Spanish ")
+    choice = input("Please choose 1-English 2-Japanese 3-Spanish ")
     
-    if choice == 1 :
+    if int(choice) == 1 :
         name = input("Please enter your name ")
         print(f"Hello {name}")
     
-    elif choice == 2 :
+    elif int(choice) == 2 :
         name = input("お名前を入力してください ")
         print(f"こんにちは {name}")
     
